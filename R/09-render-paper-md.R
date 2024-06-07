48b50425bbf537b3f690c4a8fe9242f82dc64eb4
Benjamin Trueman <40065050+bentrueman@users.noreply.github.com>
2024-06-07 11:04:15 -0300

    added readme

---
+++ R/09-render-paper-md.R
@@ -0,0 +1,34 @@
+
+# setup -------------------------------------------------------------------
+
+library("readr")
+library("stringr")
+library("rmarkdown")
+
+# modify rmd --------------------------------------------------------------
+
+# paper:
+
+path <- "Rmarkdown/paper.Rmd" # path to Rmd
+path_temp <- str_replace(path,"(.+/)(.+)", "\\1_\\2") # path to temporary modified version
+path_md <- str_replace(path, "\\.Rmd", "\\.md") # path to md doc
+path_temp_md <- str_replace(path_temp, "\\.Rmd", "\\.md") # path to temporary md doc
+
+# modify citations:
+
+read_file(path) |>
+  # move arabic numberal refs after punctuation:
+  str_replace_all("\\s+(\\[@.*?\\])(\\)?[.,;:])?", "\\2\\1") |>
+  write_file(path_temp)
+
+# render ------------------------------------------------------------------
+
+rmarkdown::render(path_temp, output_format = "md_document") # render
+
+file.rename(path_temp_md, path_md) # rename temporary md file
+
+unlink(path_temp)
+
+# SI:
+
+# rmarkdown::render("Rmarkdown/paper-si.Rmd")
--- R/functions-ppca.R
+++ R/functions-ppca.R
@@ -1,15 +1,17 @@
 
 # functions ---------------------------------------------------------------
 
-modify_stan_template <- function(dimension) {
+modify_stan_template <- function(standatalist) {
+  # get column names of censored variables:
+  index <- get_censored_column_numbers(standatalist)
   # starter code:
   code_template <- readLines(here("R/stan/ppca.stan"))
   # build additional code:
-  declare_Ncens <- build_Ncens(dimension)
-  declare_Jcens <- build_Jcens(dimension)
-  declare_U <- build_U(dimension)
-  declare_Ycens <- build_Ycens(dimension)
-  declare_yl <- build_yl(dimension)
+  declare_Ncens <- build_Ncens(index)
+  declare_Jcens <- build_Jcens(index)
+  declare_U <- build_U(index)
+  declare_Ycens <- build_Ycens(index)
+  declare_yl <- build_yl(index)
   # add to template:
   code <- c(
     code_template[3:7], # data block
@@ -33,7 +35,7 @@ modify_stan_template <- function(dimension) {
     "    yl[n] ~ normal(w * col(z, n) + mu, sigma);",
     code_template[28] # model block
   )
-  if (dimension == 0) code_template else code
+  if (length(index) == 0) code_template else code
 }
 
 build_standatalist <- function(data, components, censoring) {
@@ -51,18 +53,20 @@ build_standatalist <- function(data, components, censoring) {
 
   Ncens <- lapply(censoring, sum)
   names(Ncens) <- paste0("Ncens_y", seq(length(Ncens)))
-  Ncens <- Ncens[Ncens > 0]
 
   # positions of censored values:
   index <- seq(n)
   Jcens <- lapply(censoring, \(x) index[x])
   names(Jcens) <- paste0("Jcens_y", seq(length(Jcens)))
-  Jcens <- Jcens[Ncens > 0]
 
   # censoring limits:
   U <- mapply(\(x, y) x[y], data, censoring, SIMPLIFY = FALSE)
   names(U) <- paste0("U_y", seq(length(U)))
+
+  # remove empty list elements because no censoring for a particular element:
   U <- U[Ncens > 0]
+  Jcens <- Jcens[Ncens > 0]
+  Ncens <- Ncens[Ncens > 0]
 
   c(data_list, Ncens, Jcens, U)
 
@@ -72,37 +76,59 @@ build_standatalist <- function(data, components, censoring) {
 
 # code snippets for censoring:
 
-build_Ncens <- function(dimension) {
+get_censored_column_numbers <- function(standatalist) {
+  # get names of stan data list elements:
+  names_standata<- names(standatalist)
+  # retain names of list elements containing numbers of censored observations:
+  names_ncens <- names_standata[grepl("Ncens_y", names_standata)]
+  # retain names of list elements containing positions of censored observations:
+  names_jcens <- names_standata[grepl("Jcens_y", names_standata)]
+  # retain names of list elements containing censoring limits:
+  names_u <- names_standata[grepl("U_y", names_standata)]
+  # extract column numbers from the names extracted above:
+  columns_ncens <- regmatches(names_ncens, m = regexpr("\\d", names_ncens))
+  columns_jcens <- regmatches(names_jcens, m = regexpr("\\d", names_jcens))
+  columns_u <- regmatches(names_u, m = regexpr("\\d", names_u))
+  # all three sets of column numbers should be the same:
+  stopifnot(
+    "stan data list must include corresponding variables for position,
+    count, and upper limits of censored observations" =
+      all(duplicated(list(columns_ncens, columns_jcens, columns_u))[-1])
+  )
+  as.numeric(columns_ncens)
+}
+
+build_Ncens <- function(index) {
   sapply(
-    seq(dimension),
+    index,
     \(x) paste0("  int<lower=0> Ncens_y", x, "; // number of censored, y", x)
   )
 }
 
-build_Jcens <- function(dimension) {
+build_Jcens <- function(index) {
   sapply(
-    seq(dimension),
+    index,
     \(x) paste0("  array[Ncens_y", x, "] int<lower=1> Jcens_y", x, "; // positions of censored, y", x)
   )
 }
 
-build_U <- function(dimension) {
+build_U <- function(index) {
   sapply(
-    seq(dimension),
+    index,
     \(x) paste0("  array[Ncens_y", x, "] real U_y", x, "; // left-censoring limits, y", x)
   )
 }
 
-build_Ycens <- function(dimension) {
+build_Ycens <- function(index) {
   sapply(
-    seq(dimension),
+    index,
     \(x) paste0("  array[Ncens_y", x, "] real<lower=0, upper=U_y", x, "> Ycens_y", x, "; // estimated censored, y", x)
   )
 }
 
-build_yl <- function(dimension) {
+build_yl <- function(index) {
   sapply(
-    seq(dimension),
+    index,
     \(x) paste0("  yl[Jcens_y", x, ", ", x, "] = Ycens_y", x, ";")
   )
 }
