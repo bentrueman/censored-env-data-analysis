4cef99f869436707f91142e1b1db5bb821e477a4
Benjamin Trueman <40065050+bentrueman@users.noreply.github.com>
2024-06-10 13:32:49 -0300

    figure revisions

--- R/figure-scripts/figure-ppca.R
+++ R/figure-scripts/figure-ppca.R
@@ -33,35 +33,34 @@ w_orthonormalized <- w_estimated |>
 plot_ppca <- z_estimated |>
   mutate(site = paste("Site", biosolids_wide$site)) |>
   ggplot(aes(PC1, PC2, col = site)) +
-  # scale_color_manual(values = colour_palette[c(1, 3, 5)]) +
   scale_color_manual(values = colour_palette[c(1, 4, 7)]) +
   # annotate extreme points instead of plotting them:
   geom_label(
     data = \(x) x |>
-      filter(PC1 >= 2 | PC2 >= 3 | PC1 <= -2.5) |>
+      mutate(labels = paste0("(", round(PC1, 1), ", ", round(PC2, 1), ")")) |>
+      filter(!(PC1 < 2 & PC1 > -2 & PC2 > -2 & PC2 < 2)) |>
       mutate(
-        PC1_trunc = pmin(PC1, 1.8),
-        PC2_trunc = pmin(PC2, 3),
-        PC1_trunc = if_else(PC1_trunc < -1, pmax(-1.8), PC1_trunc)
+        across(c(PC1, PC2), ~ case_when(
+          .x >= 2 ~ Inf,
+          .x <= -2 ~ -Inf,
+          TRUE ~ .x
+        ))
       ),
-    aes(
-      x = PC1_trunc, y = PC2_trunc,
-      label = paste0("(", round(PC1, 1), ", ", round(PC2, 1), ")"),
-      fontface = "bold"
-    ),
-    label.size = 0, alpha = 0.7, size = 2.5,
+    aes(label = labels),
     hjust = "inward", vjust = "inward",
+    label.size = 0, alpha = 0.7, size = 2,
     show.legend = FALSE
   ) +
   geom_point(
     data = . %>%
-      filter(PC1 < 2, PC2 < 3, PC1 > -2.5)
+      filter(PC1 < 2 & PC1 > -2 & PC2 > -2 & PC2 < 2)
   ) +
   labs(
     x = "First latent variable (<i>z<sub>k=1</sub></i>)",
     y = "Second latent variable (<i>z<sub>k=2</sub></i>)",
     col = NULL
-  )
+  ) +
+  lims(x = c(-2, 2), y = c(-2, 2))
 
 plot_loadings <- w_orthonormalized |>
   pivot_longer(-element) |>
@@ -71,7 +70,6 @@ plot_loadings <- w_orthonormalized |>
     labels = c("First principal axis", "Second principal axis")
   ) +
   geom_line() +
-  # geom_point() +
   labs(x = NULL, y = "Loading", col = NULL) +
   theme(
     legend.direction = "horizontal",
@@ -86,7 +84,7 @@ plot_loadings <- w_orthonormalized |>
 #
 # pca$x[,1:2] |>
 #   as_tibble() |>
-#   ggplot(aes(PC1, PC2)) +
+#   ggplot(aes(PC2, PC1)) +
 #   geom_point(aes(col = factor(biosolids_wide$site))) +
 #   xlim(c(-50, 150)) +
 #   ylim(c(-50, 150))
